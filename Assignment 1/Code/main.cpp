0ea0ecec768c698892aa62eb6bbe569e6b963e8a
Trent Rand <Trentmrand@gmail.com>
2015-09-08 22:25:40 -0700

    Initial project structure
    + main c++ class
    + Runtime assets
    + Brief Report template

---
+++ Assignment 1/Code/main.cpp
@@ -0,0 +1,9 @@
+//  main.cpp
+//  A serial program that executes a sequence of commands that operate on individuals’ fitness data.
+//
+//  Valid milestone commands include:
+//  Start, End, InsertionSort, Select, Average, and Exit
+//
+//  Created by Trent Rand
+
+#include <stdio.h>
