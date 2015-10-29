efca4b35be8e79e4c841371c0b7da086d37707d5
Trent Rand <Trentmrand@gmail.com>
2015-10-29 00:08:31 -0700

    Basic 2-3 Tree Implementation

--- Project 2/Code/myAppStore.cc
+++ Project 2/Code/myAppStore.cc
@@ -29,11 +29,13 @@ struct app_info{
 	float price; // Price in $ of the application
 };
 
+/*
 struct tree{ // A binary search tree
 	struct app_info app; // Information about the application
 	struct tree* left;  // Pointer to the left subtree
 	struct tree* right;  // Pointer to the right subtree
 };
+*/
 
 struct hash_table_entry{
 	char app_name[ APP_NAME_LEN ]; // Name of the application
@@ -41,7 +43,6 @@ struct hash_table_entry{
 };
 
 // This definition of the tree is to be used for the full project deadline.
-/*
  struct tree{ // A 2-3 tree
  // In the case of a 2-node, only app_info and the left and right subtrees fields are used
  //    - app_info2 is empty and mid is NULL
@@ -54,7 +55,7 @@ struct hash_table_entry{
 	struct tree* right;  // Pointer to the right subtree; elements with keys > key of app_info2
  struct tree* parent; // A pointer to the parent may be useful
  };
- */
+ 
 
 // Allocate an array of size n of type struct categories
 struct categories* categoryList;
@@ -66,6 +67,8 @@ int hashTableLength;
 
 // Function forward declaration
 void insert(tree* node, tree** root);
+tree* searchTree(tree* node, tree* root);
+
 void printTree(struct tree* root);
 int hashASCII(char* appName);
 void insertHashEntry(tree* node);
@@ -75,33 +78,31 @@ int nextPrimeNumber(int min);
 char *operationSubjects[] = { "app", "category", "price" };
 void findHashEntry(char* appName);
 void findCategory(char* catName);
-int traversePriceFree(tree *node, int found);
+int traversePriceFree(tree* node, int& found);
 void findPriceFree();
-void rangePrice(char* category);
-int traverseRangePrice(tree *node, int found, int low, int high);
-void rangeApp(char* category);
-int traverseRangeApp(tree *node, int found, char* low, char* high);
+void rangePrice(char* category, float low, float high);
+int traverseRangePrice(tree *node, int& found, int low, int high);
+void rangeApp(char* category, char* low, char* high);
+int traverseRangeApp(tree *node, int& found, char* low, char* high);
 
 int main()
 {
 	// Allocate an array of size n of type struct categories
-	cin >> categoryListLength;
+	cin >> categoryListLength; cin.ignore();
 	categoryList = new categories[categoryListLength]; // Deallocate on exit
-	
-	// Clear input stream, fixes line 73 skipping one input
-	cin.ignore();
 	
 	// Allocate an array of size n of type struct categories
 	for (int i = 0; i < categoryListLength; i++) {
-		string title;
-		getline(cin, title);
-		strcpy(categoryList[i].category, title.c_str());
+		char title [ CAT_NAME_LEN ];
+		cin.getline(title, CAT_NAME_LEN);
+		strcpy(categoryList[i].category, title);
 		// For safety, set all <root> nodes to NULL
 		categoryList[i].root = NULL;
 	}
 	
-	// Populate myAppStore categories BST dwith m applications
-	int numberOfApplications; cin >> numberOfApplications;
+	// Populate myAppStore categories BST with m applications
+	int numberOfApplications;
+	cin >> numberOfApplications; cin.ignore();
 	
 	// Intialize <hashTable> to size k that is the first prime number greater than 2 * <numberOfApplications>.
 	hashTableLength = nextPrimeNumber(2*numberOfApplications);
@@ -116,24 +117,23 @@ int main()
 	for (int i = 0; i<numberOfApplications; i++) {
 		struct tree* node = new tree();
 
-//		printf("Enter category for application %d\n", i); //TODO remove this, debug only
+//		fprintf(stderr, "Enter category for application %d\n", i); //TODO remove this, debug only
 		
 		// Read in category name from next line of cin
 		char categoryName[ CAT_NAME_LEN ];
-		cin.ignore();
 		cin.getline(categoryName, CAT_NAME_LEN);
-		
-//		printf("Application is being created in category: %s\n", categoryName); //TODO remove this, debug only
+
+//		fprintf(stderr, "Application is being created in category: %s\n", categoryName); //TODO remove this, debug only
 		
 		int desiredCategoryIndex = -1;
-//		printf("Searching for category: %s\n", categoryName); //TODO remove this, debug only
+//		fprintf(stderr, "Searching for category: %s\n", categoryName); //TODO remove this, debug only
 		// Search the array of categories, to find the position matching the category of the application.
 		for (int index = 0; index <= categoryListLength; index++) {
 			
 			// Compare category name at <index> with desired category name
 			if (strcmp(categoryName, categoryList[index].category) == 0) {
 				desiredCategoryIndex = index;
-//				printf("Found desired category at index: %d\n", desiredCategoryIndex); //TODO remove this, debug only
+//				fprintf(stderr, "Found desired category at index: %d\n", desiredCategoryIndex); //TODO remove this, debug only
 				break; // No need to search further
 			}
 		}
@@ -142,13 +142,17 @@ int main()
 			printf("Category not found.");
 		} else {
 			// Read the information of the application
-//			printf("Enter application information:\n"); //TODO remove this, debug only
+//			fprintf(stderr, "Enter application information:\n"); //TODO remove this, debug only
 			strcpy(node->app.category, categoryName);
 			cin.getline(node->app.app_name, APP_NAME_LEN);
-			cin >> node->app.version;
-			cin >> node->app.size;
-			cin >> node->app.units;
-			cin >> node->app.price;
+			
+			cin >> node->app.version; cin.ignore();
+			
+			cin >> node->app.size; cin.ignore();
+			
+			cin >> node->app.units; cin.ignore();
+
+			cin >> node->app.price; cin.ignore();
 			
 			// Insert the node into the search tree for the desired application category in asecending alphabetic order
 			insert(node, &categoryList[desiredCategoryIndex].root);
@@ -157,38 +161,105 @@ int main()
 			insertHashEntry(node);
 			
 		}
-		// printTree(categoryList[desiredCategoryIndex].root); // TODO remove this, debug only
+		printTree(categoryList[desiredCategoryIndex].root); // TODO remove this, debug only
+
 	}
-	
+
+
 	// Process q queries and updates
-	int q; cin >> q;
+	int q;
+	cin >> q; cin.ignore();
+
 	char operation[OPERATION_LEN];
 	
 	for (int i = 0; i < q; i++) {
 		// Read in first keyword, the operation to perform
-		cin >> operation;
+		cin >> operation; cin.ignore();		
+
 		if (strcmp(operation, "find") == 0) { // find <subject> <key>
-			char subject[9]; cin >> subject;
+			char subject[9];
+			cin >> subject; cin.ignore();
+
+
 			if (strcmp(subject, operationSubjects[0]) == 0) { // find app <appName>
-				char appName[APP_NAME_LEN]; cin >> appName;
+				char appName[APP_NAME_LEN];
+				cin.getline(appName, APP_NAME_LEN);
 				findHashEntry(appName);
 			} else if (strcmp(subject, operationSubjects[1]) == 0) { // find category <catName>
-				char catName[CAT_NAME_LEN]; cin >> catName;
+				char catName[CAT_NAME_LEN];
+				cin.getline(catName, CAT_NAME_LEN);
 				findCategory(catName);
 			} else if (strcmp(subject, operationSubjects[2]) == 0) { // find price free
-				char price[5]; cin >> price; // Clear buffer, protentionally find specific price
+				char price[5];
+				cin >> price; cin.ignore(); // Clear buffer, protentionally find specific price
 				findPriceFree();
 			}
 		} else if (strcmp(operation, "range") == 0) {
-			char category[ CAT_NAME_LEN ]; cin >> category; // category of <categoryList>
-			char subject [ 9 ]; cin >> subject; // price or app
+			string rangeOperation;
+			
+			char category[ CAT_NAME_LEN ]; // Store the category name
+			char subject [ 9 ]; // Store the subject, price or app
+
+			getline(cin, rangeOperation);
+			int pos = -1;
+			pos = rangeOperation.find("price");
+			if (pos != -1) {
+				// Price
+				strcpy(subject, "price");
+				// Erase price from rangeOperation
+				rangeOperation.erase(pos, 6);
+			} else {
+				// App
+				pos = rangeOperation.find("app");
+				strcpy(subject, "app");
+				// Erase app from rangeOperation
+				rangeOperation.erase(pos, 4);
+			}
+			strcpy(category, rangeOperation.substr(0, pos-1).c_str()); // extract category from <rangeOperation>
+			rangeOperation.erase(0, pos); // erase category from <rangeOperation>
+			
 			if (strcmp(subject, operationSubjects[2]) == 0) { // range <category> price <low> <high>
-				rangePrice(category);
+				float low;
+				float high;
+				
+				int spacePosition;
+				spacePosition = rangeOperation.find(' ');
+				high = stof(rangeOperation.substr(pos-1));
+				rangeOperation.erase(pos-1);
+				low = stof(rangeOperation);
+				
+				rangePrice(category, low, high);
 			} else if (strcmp(subject, operationSubjects[0]) == 0) { // range <category> app <low> <high>
-				rangeApp(category);
+				char low[ APP_NAME_LEN ];
+				char high[ APP_NAME_LEN ];
+
+				int spacePosition;
+				spacePosition = rangeOperation.find(' ');
+				
+				// Copy in low
+				strcpy(low, rangeOperation.substr(0, spacePosition).c_str());
+				rangeOperation.erase(0, spacePosition+1);
+				
+				// Copy in high
+				strcpy(high, rangeOperation.c_str());
+
+				rangeApp(category, low, high);
 			}
 		} else if (strcmp(operation, "delete") == 0) {
+			string rangeOperation;
+			getline(cin, rangeOperation);
 			
+			int categoryFoundAt = -1;
+			int i = 0;
+			while (categoryFoundAt == -1 && i < categoryListLength) {
+				categoryFoundAt = rangeOperation.find(categoryList[i].category);
+				i++;
+			}
+			if (categoryFoundAt == -1) {
+				cout << "Application not found; unable to delete." << endl;
+			} else {
+				rangeOperation.erase(categoryFoundAt, strlen(categoryList[i].category)-1);
+			}
 		}
 	}
 	
@@ -218,13 +289,69 @@ void insert(tree* node, tree** root) {
 	}
 }
 
+//// Insert passed node into passed binary search tree
+//void insert(tree* node, tree** root) {
+//	if (*root == NULL) {
+//		// Root is empty, create new node
+//		*root = node;
+//		(*root)->left = NULL;
+//		(*root)->mid = NULL;
+//		(*root)->right = NULL;
+//	} else {
+//		tree* insertionNode;
+//		insertionNode = searchTree(node, (*root));
+//	}
+//}
+
+tree* searchTree(tree* node, tree* root) {
+	if (root == NULL || (strcmp(root->app.app_name,  "") == 0) || (strcmp(root->app2.app_name,  "") == 0)) {
+		// Root is empty, or has empty app container, start insertion
+		return root;
+	}
+	else {
+		if (strcmp(node->app.app_name, root->right->app.app_name) < 0) {
+			// Traverse left
+			cout << "Traverse left" << endl;
+			searchTree(node, root->left);
+		} else if (strcmp(node->app.app_name, root->right->app2.app_name) > 0) {
+			// Traverse right
+			cout << "Traverse right" << endl;
+			searchTree(node, root->right);
+
+		} else {
+			// Traverse to mid
+			cout << "Traverse mid" << endl;
+			searchTree(node, root->mid);
+
+		}
+	}
+}
+
 // A utility function to do inorder traversal of BST
 void printTree(struct tree* root) {
-	if (root != NULL)
-	{
+	if (root == NULL) return;
+	
+	if (root->left == NULL && root->mid == NULL && root->right == NULL) {
+		// Root is a leaf
+		printf("%s\n", root->app.app_name);
+		
+	} else if ((root->left != NULL && root->right != NULL && root->mid == NULL) || (root->left != NULL && root->right == NULL && root->mid != NULL)
+			   || (root->left == NULL && root->right != NULL && root->mid != NULL)) {
+		// Root is a 2-node
 		printTree(root->left);
 		printf("%s\n", root->app.app_name);
 		printTree(root->right);
+		
+	} else {
+		// Root is a 3-node
+		printTree(root->left);
+		printf("%s\n", root->app.app_name);
+		printTree(root->mid);
+		if (strcmp(root->app2.app_name, "") != 0) {
+		printf("%s\n", root->app2.app_name);
+		}
+		printTree(root->right);
+
 	}
 }
 
@@ -333,7 +460,8 @@ void findPriceFree() {
 		// Organize output by category
 		cout << "Category: " << categoryList[i].category << endl;
 		tree* root = categoryList[i].root;
-		int found = traversePriceFree(root, 0);
+		int found = 0;
+		traversePriceFree(root, found);
 		if (found == 0) {
 			cout << "No free applications found." << endl;
 		}
@@ -341,20 +469,23 @@ void findPriceFree() {
 }
 
 // Traverse tree, if price == 0, printout and found++
-int traversePriceFree(tree* node, int found) {
-	if (node == NULL) return 0;
+int traversePriceFree(tree* node, int& found) {
+	if (node == NULL) return found;
+	
 	traversePriceFree(node->left, found);
+	
 	if (node->app.price == 0) {
 		cout << node->app.app_name << endl;
 		++found;
 	}
+	
 	traversePriceFree(node->right, found);
 	
 	return found;
 }
 
 // Find and print all apps in category whos price is between low and high criteria
-void rangePrice(char* category) {
+void rangePrice(char* category, float low, float high) {
 	int desiredIndex;
 	// Look through category list for appropriate index
 	for (int i = 0; i < categoryListLength; i++) {
@@ -364,19 +495,16 @@ void rangePrice(char* category) {
 		}
 	}
 	
-	// At categoryList[desiredIndex], look for app between <low> and <high>
-	int low, high;
-	cin >> low; cin >> high;
-	
 	tree* root = categoryList[desiredIndex].root;
-	int found = traverseRangePrice(root, 0, low, high);
+	int found = 0;
+	traverseRangePrice(root, found, low, high);
 	if (found == 0) {
 		cout << "No applications found for given range." << endl;
 	}
 }
 
 // Traverse tree, if low <= price <= high, print app_name
-int traverseRangePrice(tree *node, int found, int low, int high) {
+int traverseRangePrice(tree *node, int& found, int low, int high) {
 	if (node == NULL) return 0;
 	traverseRangePrice(node->left, found, low, high);
 	if (node->app.price >= low && node->app.price <= high) {
@@ -389,8 +517,9 @@ int traverseRangePrice(tree *node, int found, int low, int high) {
 }
 
 // Find and print all apps in category whos price is between low and high criteria
-void rangeApp(char* category) {
+void rangeApp(char* category, char* low, char* high) {
 	int desiredIndex;
+
 	// Look through category list for appropriate index
 	for (int i = 0; i < categoryListLength; i++) {
 		if (strcmp(categoryList[i].category, category) == 0) {
@@ -399,19 +528,17 @@ void rangeApp(char* category) {
 		}
 	}
 	
-	// At categoryList[desiredIndex], look for app_name between <low> and <high>
-	char low[25], high[25];
-	cin >> low; cin >> high;
-	
 	tree* root = categoryList[desiredIndex].root;
-	int found = traverseRangeApp(root, 0, low, high);
+	int found = 0;
+	traverseRangeApp(root, found, low, high);
+
 	if (found == 0) {
 		cout << "No applications found for given range." << endl;
 	}
 }
 
 // Traverse tree, if low <= price <= high, print app_name
-int traverseRangeApp(tree *node, int found, char* low, char* high) {
+int traverseRangeApp(tree *node, int& found, char* low, char* high) {
 	if (node == NULL) return 0;
 	traverseRangeApp(node->left, found, low, high);
 	if (strcmp(node->app.app_name, low) >= 0 && strcmp(node->app.app_name, high) <= 0) {
