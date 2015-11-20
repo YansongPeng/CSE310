4e33cccc8d8990e57a7e0d73817a6a1dc6ec3599
Trent Rand <Trentmrand@gmail.com>
2015-11-20 05:14:33 -0700

    Components Operation Implemented

--- Project 3/Code/p3.cc
+++ Project 3/Code/p3.cc
@@ -12,10 +12,13 @@ void printList(list<int>* adjlist, int listLength);
 void degreeDistribution();
 void shortestPath(int s, int t);
 void diameter();
-void components();
+void components(int listLength);
+void depthFirstSearch(int vertex, bool visited[]);
 
 /* Global Variables */
 list<int> *adjacencyList; // Stores our vertex-edge adjacency list
+bool *visited;
+int componentSize = 0;
 
 int main()
 {
@@ -25,7 +28,7 @@ int main()
 	cin >> n; cin.ignore(); // read n, clear buffer
 	
 	adjacencyList = new list<int>[n]; // Allocate <adjacencyList> with <n> vertices
-	
+
 	int m; // m = |E|, the number of edges in the graph. (# of paths/edges)
 	cin >> m; cin.ignore(); // read m, clear buffer
 	
@@ -39,8 +42,8 @@ int main()
 
 		// If there are no self-loops, i.e., u != v, insert them into the graph.
 		if (!(u == v)) {
-			adjacencyList[u].push_back(v); // Add U:V, as given through input
-			adjacencyList[v].push_back(u); // Add the opposite path, V:U, since this a non-directed adjacency graph
+			adjacencyList[u].push_back(v); // Add (u, v), as given through input
+			adjacencyList[v].push_back(u); // Add the opposite path, (v, u), since this a non-directed adjacency graph
 		}
 	}
 	
@@ -64,11 +67,14 @@ int main()
 		} else if (operation == "diameter") {
 			diameter();
 		} else if (operation == "components") {
-			components();
+			components(n);
 		} else {
 			cout << "Invalid operation." << endl;
 		}
 	}
+	
+	// Memory Management
+	delete [] adjacencyList;
 }
 
 /* Prints the adjacency list, for debugging */
@@ -97,22 +103,67 @@ void degreeDistribution() {
  *  V: destination vertex
  */
 void shortestPath(int s, int t) {
-
+	cout << "Shortest Path operation will be implemented in complete project submission." << endl;
 }
 
 /* Prints the diameter of the graph defined as the length of the longest shortest path.
  * Uses Floyd-Warshall all-pairs shortest path algorithm.
  */
 void diameter() {
-
+	cout << "Diameter operation will be implemented in complete project submission." << endl;
 }
 
 /* Prints the number of connected components in the graph and their size.
  * Size = number of vertices in each component.
- * Uses breadth-first search, marking and counting nodes as the search proceeds.
- * If any unmarked vertices exist, a new breadth-first search is started at unmarked node.
+ * Uses depth-first search, marking and counting nodes as the search proceeds.
+ * If any unmarked vertices exist, a new depth-first search is started at unmarked node.
  * Repeats until all nodes are marked.
  */
-void components() {
+void components(int listLength) {
+	int components = 0; // Store count of components
+	int componentsSize[listLength]; // Store component size, with a maximum of <listLength> components
+	
+	visited = new bool[listLength]; // Track visited vertices
+	
+	// Initialize all vertices in <visited> to false
+	for(int v = 0; v < listLength; v++) {
+		visited[v] = false;
+	}
+	
+	// depth-first search for connected components
+	for (int v = 0; v < listLength; v++)
+	{
+		if (visited[v] == false)
+		{
+			depthFirstSearch(v, visited); // Recur for all adjacent vertices
+			componentsSize[components] = componentSize;
+			componentSize=0;
+			components++;
+		}
+	}
+	
+	// Print the number of connected components in the graph and their size
+	cout << "The number of connected components is " << components << endl;
+	for (int i = 0; i<components; i++) {
+		cout << "Component " << i << " has " << componentsSize[i] << " vertices." << endl;
+	}
+	
+	// Memory Management
+	delete [] visited;
+}
 
+/* Utility method for Depth First Search of adjacency list.
+ *  Marks node <vertex> as visited and prints
+ */
+void depthFirstSearch(int vertex, bool visited[]) {
+	visited[vertex] = true; // Set vertecy as visited
+	componentSize++; // Increase size count of component
+	
+//	cout << "Visting: " << vertex << endl; // Debug, see where the depth first search visits
+	
+	for(list<int>::iterator it = adjacencyList[vertex].begin(); it != adjacencyList[vertex].end(); ++it) {
+		if (visited[*it] == false) {
+			depthFirstSearch(*it, visited); // Recurse the tree at all non-visited vertecies
+		}
+	}
 }
